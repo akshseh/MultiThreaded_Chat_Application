3e6ec4b87f803a990786911709aa64981c9c7bef
Akarsha Sehwag <akarsha15010@iiitd.ac.in>
2017-09-10 04:17:17 +0530

    Update client.java

--- client.java
+++ client.java
@@ -5,13 +5,33 @@ import java.net.*;
 import java.io.*;
 
 
-public class client
+public class client extends Thread 
 {
 	static Socket soc = null;
 	static DataOutputStream out = null;
 	static DataInputStream in = null;
+	static PrintStream priOstr = null;
 	static BufferedReader br = null;
-	static InputStreamReader istrRead = null;
+	//static InputStreamReader istrRead = null;
+	static int flag = 0;
+		
+
+	public void run(){
+		try{
+			String str = in.readLine();
+			while(str!=null && str.indexOf("close")< 0){
+				//System.out.println(str);
+				//System.out.println("lopo "+str.indexOf("close"));
+				str = in.readLine();
+				//System.out.println("lopoo");
+			}
+		flag = 1;
+		//System.out.println("lopoo");
+		}
+		catch(IOException r){
+			System.out.println("Exceptionn");
+		}
+	}
 
 	public static void main(String[] args) throws Exception
 	{
@@ -24,26 +44,29 @@ public class client
 			//Scjset
 			in = new DataInputStream(soc.getInputStream());	
 			//sends output to socket
-			out = new DataOutputStream(soc.getOutputStream());
+			//out = new DataOutputStream(soc.getOutputStream());
+			priOstr = new PrintStream(soc.getOutputStream());
+			//ill
 			br = new BufferedReader(new InputStreamReader(System.in));
 		} 
-		catch(Exception e)
+		catch(IOException e)
 		{
-			System.out.println(e);
+			System.out.println("IO problem ");
 		}
-		String st = "";
-		String st2 = "";
 
-		while(!st.equals("close"))
+		//String st = "";
+		//String st2 = "";
+		new Thread(new client()).start();
+		while(flag == 0)
 		{
 			try
 			{
-				st = br.readLine();
-				out.writeUTF(st);
-				//Flishshsh
-				out.flush();
-				st2 = in.readUTF();
-				System.out.println("Message from server: " + st2);
+				//st = br.readLine();
+				//new Thread(new client()).start();
+				priOstr.println(br.readLine());
+				//out.writeUTF(st);
+				//st2 = in.readUTF();
+				//System.out.println("Message from server: " + st2);
 			}
 			catch(Exception ex)
 			{
@@ -54,7 +77,7 @@ public class client
 		System.out.println("Connection closed.");
 		try
 		{
-			out.close();
+			priOstr.close();
 			in.close();
 			soc.close();
 		}
